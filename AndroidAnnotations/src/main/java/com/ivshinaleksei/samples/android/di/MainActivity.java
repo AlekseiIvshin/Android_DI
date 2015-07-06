aa702b1b613283e5c82ec32a55e95d91c5f1f12c
Aleksei Ivshin <ivshinalekseim@mail.ru>
2015-07-06 23:42:34 +0400

    Adds roboguice sample module.

--- AndroidAnnotations/src/main/java/com/ivshinaleksei/samples/android/di/MainActivity.java
+++ AndroidAnnotations/src/main/java/com/ivshinaleksei/samples/android/di/MainActivity.java
@@ -62,6 +62,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadWeatherData();
     }
 
     @Background(id = LOAD_WEATHER_TASK, delay = 6000)
